d5b0d01e43224ad55d15c3792e9522e5fc943f0b
tignioj <859897994@qq.com>
2024-08-05 07:08:31 +0800

    添加重置计时器方法

--- myutils/timerutils.py
+++ myutils/timerutils.py
@@ -1,8 +1,7 @@
 import time
+
+
 class Timer:
-    """
-    计时器，创建一个计时器对象，指定时间后返回True，否则返回False
-    """
     def __init__(self, duration):
         self.duration = duration
         self.start_time = None
@@ -19,3 +18,70 @@ class Timer:
             return True
         else:
             return False
+
+    def reset(self):
+        self.start_time = None
+
+class RateLimiter:
+    def __init__(self, interval):
+        """
+        初始化RateLimiter实例
+        :param interval: 两次执行之间的最小间隔时间（以秒为单位）
+        """
+        self.interval = interval
+        self.last_executed = 0
+
+    def execute(self, func, *args, **kwargs):
+        """
+        尝试执行指定的函数
+
+        :param func: 要执行的函数
+        :param args: 传递给函数的参数
+        :param kwargs: 传递给函数的关键字参数
+        :return: 如果在指定时间内执行成功，则返回True；否则返回False
+        """
+        current_time = time.time()
+        if current_time - self.last_executed >= self.interval:
+            self.last_executed = current_time
+            func(*args, **kwargs)
+            return True
+        else:
+            return False
+
+
+def demo1():
+    # 2秒钟输出一次
+    t = Timer(3)
+    while True:
+        try:
+            if t.check():
+                print('Time elapsed: ', time.time() - t.start_time)
+                t.reset()
+        except:
+            t.start()
+        time.sleep(0.1)
+def demo2():
+    t = Timer(3)
+    while True:
+        if t.start_time is None:
+            t.start()
+        else:
+            if t.check():
+                print('Time elapsed: ', time.time() - t.start_time)
+                t.reset()
+        time.sleep(0.1)
+
+def demo3():
+    t = None
+    while True:
+        if t is None:
+            t = Timer(3)
+            t.start()
+        if t.check():
+            print('Time elapsed: ', time.time() - t.start_time)
+            t = None
+
+
+if __name__ == '__main__':
+    demo3()
+
