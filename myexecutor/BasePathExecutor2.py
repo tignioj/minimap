f8ea2c9244d659d28de1ac539e289dcb8f667699
tignioj <859897994@qq.com>
2024-08-09 02:48:38 +0800

    重构：移动逻辑优化

--- myexecutor/BasePathExecutor2.py
+++ myexecutor/BasePathExecutor2.py
@@ -1,21 +1,15 @@
-import random
-import sys
 import threading
 import time
 import cv2
-import json
-import logging
-import numpy as np
 from controller.BaseController import BaseController
 from collections import deque
 from controller.MapController2 import MapController
 from controller.OCRController import OCRController
-from capture.genshin_capture import GenShinCapture
-from myexecutor.executor_utils import point1_near_by_point2
+from myutils.executor_utils import point1_near_by_point2
+from myutils.jsonutils import getjson_path_byname
 from myutils.configutils import cfg, PROJECT_PATH
 from myutils.timerutils import RateLimiter
 from mylogger.MyLogger3 import MyLogger
-from typing import List
 
 logger = MyLogger('path_executor')
 
@@ -60,7 +54,7 @@ class BasePathExecutor(BaseController):
 
     def __init__(self, json_file_path, debug_enable=False):
         super().__init__(debug_enable=debug_enable)
-        from myexecutor.executor_utils import load_json
+        from myutils.jsonutils import load_json
         json_map = load_json(json_file_path)
         self.country = json_map['country'] # 传送到什么区域
         self.target_name = json_map['name'] # 目标名称
@@ -126,6 +120,11 @@ class BasePathExecutor(BaseController):
         self.rate_limiter1_history = RateLimiter(1)  # 一秒钟之内只能执行一次
         self.rate_limiter5_debug_print = RateLimiter(5)  # 5秒内只能执行一次
 
+        self.rate_limiter_press_e = RateLimiter(1)
+        self.rate_limiter_press_z = RateLimiter(1)
+        self.rate_limiter_press_jump = RateLimiter(1)
+        self.rate_limiter_press_dash = RateLimiter(1)
+
 
     def _thread_object_detection(self):
         pass
@@ -161,6 +160,40 @@ class BasePathExecutor(BaseController):
             self.debug('疯狂按下f')
             self.crazy_f()
 
+    def do_action_if_moving_stuck(self):
+        self.kb_press_and_release('d')  # 避免卡住
+        self.kb_press_and_release(self.Key.space)  # 避免卡住
+        self.kb_press_and_release('a')  # 避免卡住
+        self.kb_press_and_release(self.Key.space)  # 避免卡住
+        self.kb_press("w")
+
+    def do_action_if_timeout(self):
+        self.logger.debug('点位执行超时, 跳过该点位')
+        self.kb_press_and_release("x")  # 避免攀爬
+        self.kb_press_and_release(self.Key.space)  # 避免卡住
+        self.kb_press_and_release('s')  # 避免卡住
+        self.kb_press_and_release('a')  # 避免卡住
+        self.kb_press_and_release('d')  # 避免卡住
+        self.kb_press_and_release('w')  # 避免卡住
+
+
+    def __do_move(self, coordinates):
+        # 开技能
+        if self.enable_loop_press_z: self.rate_limiter_press_z.execute(self.kb_press_and_release, 'z')
+        if self.enable_loop_press_e: self.rate_limiter_press_e.execute(self.kb_press_and_release, 'e')
+
+        # 是否卡住动的判断
+        self.rate_limiter1_history.execute(self.position_history.append, self.current_position)
+        total_displacement = self.calculate_total_displacement()  # 8秒内的位移
+        if total_displacement < self.stuck_movement_threshold: raise MovingStuckException(f"8秒内位移平均值为{total_displacement}, 判定为卡住了！")
+
+        # 转向: 注意，update_state()线程永远保证self.positions在首次赋值之后，不会是空值
+        rot = self.get_next_point_rotation(coordinates)
+        if rot: self.to_degree(self.get_next_point_rotation(coordinates))
+
+        # 前进: 注意要先转向后再前进，否则可能出错
+        self.kb_press("w")
+
     # 移动
     # 异常：原地踏步
     # 类型：途径点、调查点
@@ -169,87 +202,46 @@ class BasePathExecutor(BaseController):
     def move(self, coordinates, small_step_enable=False):
         if self.stop_listen: return
         point_start_time = time.time()
-
-
-        # 是否接近下一个点位的距离阈值
-        if small_step_enable: near_by_threshold = self.target_nearby_threshold
-        else: near_by_threshold = self.path_point_nearby_threshold
-
-        # 当接近目的地时，调整更小的阈值, 使得小碎步可以更精准走到目的地
-        rate_limiter_debugprint = RateLimiter(1)
-
-        rate_limiter_press_e = RateLimiter(1)
-        rate_limiter_press_z = RateLimiter(1)
-        rate_limiter_press_jump = RateLimiter(1)
-        rate_limiter_press_dash = RateLimiter(1)
-
-        running_small_step = False  # 当距离过远的时候，不需要小碎步行动
-        while not point1_near_by_point2(self.current_position, coordinates, near_by_threshold):
-            if self.stop_listen: return
-            t = time.time()
-
-            if self.enable_loop_press_z: rate_limiter_press_z.execute(self.kb_press_and_release, 'z')
-            if self.enable_loop_press_e: rate_limiter_press_e.execute(self.kb_press_and_release, 'e')
-
+        # 大距离快速接近
+        while not point1_near_by_point2(self.current_position, coordinates, self.path_point_nearby_threshold):
             try:
-                if time.time() - point_start_time >= self.move_next_point_allow_max_time:
-                    raise MovingTimeOutException("跑点超时！")
-                pos = self.current_position
-                if not pos:
-                    self.log("获取地址失败，正在等待刷新地址, 松开wsad")
-                    self.kb_release('w')
-                    self.kb_release('s')
-                    self.kb_release('a')
-                    self.kb_release('d')
-                    time.sleep(1)
-                    continue
-
-                if point1_near_by_point2(pos, coordinates, self.path_point_nearby_threshold):
-                    self.on_nearby(coordinates)
-                    running_small_step = True
-
-                self.rate_limiter1_history.execute(self.position_history.append, self.current_position)
-                # self.position_history.append(self.current_position)  # 记录历史路径
-                total_displacement = self.calculate_total_displacement()  # 8秒内的位移
-                if total_displacement < self.stuck_movement_threshold: raise MovingStuckException(f"8秒内位移平均值为{total_displacement}, 判定为卡住了！")
-
-                rate_limiter_debugprint.execute(self.debug, f"执行{coordinates}点位,已用{time.time() - point_start_time}秒")
-                rot = self.get_next_point_rotation(coordinates)
-                if rot: self.to_degree(rot)
-
+                if self.stop_listen: return
+                # 执行移动
+                self.__do_move(coordinates)
+                # 行动:如果距离点位超过20个像素值，则可以采取冲刺+跳跃操作
                 if not point1_near_by_point2(self.current_position, coordinates, 20):
-                    # BUG: 必须先冲刺后，跳跃才生效
-                    if self.enable_dash: rate_limiter_press_dash.execute(self.mouse_right_click)
+                    # 游戏特性: 必须先冲刺后再过0.几秒后，跳跃才生效
+                    if self.enable_dash: self.rate_limiter_press_dash.execute(self.mouse_right_click)
                     if self.enable_loop_jump:
                         if self.enable_dash: time.sleep(0.005)  # 防止执行时间过短
-                        rate_limiter_press_jump.execute(self.kb_press_and_release, self.Key.space)
+                        self.rate_limiter_press_jump.execute(self.kb_press_and_release, self.Key.space)
+                time.sleep(self.UPDATE_POSITION_INTERVAL)  # 跑点频率保持和点位更新频率相同
+            except MovingStuckException as e:
+                self.logger.error(e)
+                self.do_action_if_moving_stuck()
+            except MovingTimeOutException as e:
+                self.logger.error(e)
+                self.do_action_if_timeout()
 
-                self.kb_press("w")
-                if small_step_enable and running_small_step and self.allow_small_steps:
+        # 当接近目的地时，调整更小的阈值, 使得小碎步可以更精准走到目的地
+        if small_step_enable and self.allow_small_steps:
+            while not point1_near_by_point2(self.current_position, coordinates, self.target_nearby_threshold):
+                if self.stop_listen: return
+                try:
+                    self.__do_move(coordinates)
                     time.sleep(0.08)
                     self.debug("小碎步松开w")
                     self.kb_release('w')
-                    # self.kb_press_and_release('d')
-                time.sleep(self.UPDATE_POSITION_INTERVAL)
+                    time.sleep(self.UPDATE_POSITION_INTERVAL)
+                    self.on_nearby(coordinates)
+                except MovingStuckException as e:
+                    self.logger.error(e)
+                    self.do_action_if_moving_stuck()
+                except MovingTimeOutException as e:
+                    self.logger.error(e)
+                    self.do_action_if_timeout()
 
-            except MovingStuckException as e:
-                self.debug(e)
-                self.kb_press_and_release('d')  # 避免卡住
-                self.kb_press_and_release(self.Key.space)  # 避免卡住
-                self.kb_press_and_release('a')  # 避免卡住
-                self.kb_press_and_release(self.Key.space)  # 避免卡住
-                self.kb_press("w")
-            except MovingTimeOutException:
-                self.logger.debug('点位执行超时, 跳过该点位')
-                self.kb_press_and_release("x")  # 避免攀爬
-                self.kb_press_and_release(self.Key.space)  # 避免卡住
-                self.kb_press_and_release('s')  # 避免卡住
-                self.kb_press_and_release('a')  # 避免卡住
-                self.kb_press_and_release('d')  # 避免卡住
-                self.kb_press_and_release('w')  # 避免卡住
-                return
-
-            # self.logger.debug('cost time: ' + str(time.time()-t))
+        self.logger.debug(f'跑点{coordinates}用时：{time.time() - point_start_time}')
         self.kb_release('w')
 
 
@@ -271,7 +263,7 @@ class BasePathExecutor(BaseController):
         self.rate_limiter5_debug_print.execute(self.debug, msg)
 
     def get_next_point_rotation(self, next_point):
-        from myexecutor.executor_utils import calculate_angle
+        from myutils.executor_utils import calculate_angle
         if self.current_position and next_point:
             nextp = (next_point[0], next_point[1])
             x0, y0 = self.current_position[0], self.current_position[1]
@@ -289,7 +281,7 @@ class BasePathExecutor(BaseController):
             time.sleep(0.3)
             if len(self.points) > 0:
                 # win_name = 'path viewer'
-                img = get_points_img_live(self.points, self.target_name, radius=cfg.get('path_viewer_width', 500))
+                img = get_points_img_live(self.points, self.target_name, width=self.path_viewer_width, scale=2)
                 if img is None: continue
                 cv2.imshow(win_name, img)
                 cv2.moveWindow(win_name, 10, 10)
@@ -335,10 +327,8 @@ class BasePathExecutor(BaseController):
         if len(self.points) < 1:
             self.logger.warning(f"空白路线, 跳过")
             return
-
         # 传送的时候可以顺便缓存局部地图，因此把传送放在第一行
-        self.map_controller.transform((self.points[0].x, self.points[0].y),
-                                      self.country, create_local_map_cache=True)
+        self.map_controller.transform((self.points[0].x, self.points[0].y), self.country, create_local_map_cache=True)
         # 开始更新位置
         thread_update_state = threading.Thread(target=self._thread_update_state)
         thread_update_state.start()
@@ -368,8 +358,10 @@ class BasePathExecutor(BaseController):
                 self.log("当前点位距离下一个点位过远，可能是由于角色死亡被传送, 提前终止{}")
                 return False
 
-            small_step_enable = point.type == self.object_to_detect or point.type == 'end'
-            point.x -= 5.84
+            small_step_enable = point.type == self.object_to_detect
+            # point.x -= 5.84
+            # point.y -= 3
+            # point.x -= 0.6
             self.move((point.x, point.y), small_step_enable)
             self.debug("已到达", point)
 
@@ -415,19 +407,14 @@ def execute_all():
         # print("30秒后进入睡眠")
         # time.sleep(30)
 
-def getjson(filename):
-    # 获取当前脚本所在的目录
-    target = filename.split("_")[0]
-    from myutils.configutils import resource_path
-    # 拼接资源目录的路径
-    file = os.path.join(resource_path,'pathlist',target, filename)
-    return file
 
 if __name__ == '__main__':
     # 测试点位
-    import json, os
+    import os
     # execute_one(getjson('甜甜花_枫丹_中央实验室遗址_2024-07-31_07_01_37.json'))
-    execute_one(getjson('jiuguan_蒙德_wfsd_20240808.json'))
-    # ew.execute_one(getjson('甜甜花_枫丹_中央实验室遗址_test_2024-08-07_10_28_37.json'))
+    # execute_one(getjson_path_byname('jiuguan_蒙德_wfsd_20240808.json'))
+    # execute_one(getjson_path_byname('jiuguan_枫丹_tiantianhua_20240808.json'))
+    # execute_one(getjson_path_byname('甜甜花_枫丹_中央实验室遗址_test_2024-08-08_12_12_43.json'))
+    execute_one(getjson_path_byname('风车菊_蒙德_清泉镇_2024-08-08_14_46_25.json'))
     # execute_all()
 
