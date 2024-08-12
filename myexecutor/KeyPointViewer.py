7c03199f3b06b71dc747dd894360d720281a58f7
tignioj <859897994@qq.com>
2024-08-12 18:23:38 +0800

    删除不必要参数

--- myexecutor/KeyPointViewer.py
+++ myexecutor/KeyPointViewer.py
@@ -7,11 +7,11 @@ from matchmap.minimap_interface import MinimapInterface
 from myutils.jsonutils import getjson_path_byname
 from myexecutor.BasePathExecutor2 import Point,BasePathExecutor
 
-def show_points(points, object_to_detect=None, width=1024, scale=None):
+def show_points(points, width=1024, scale=None):
     if len(points) < 1:
         return
 
-    region_img = get_points_img(points,object_to_detect, width,scale=scale)
+    region_img = get_points_img(points,width,scale=scale)
     # region_img = cv2.resize(region_img, None, fx=0.5, fy=0.5)
     from PIL import Image
     img = Image.fromarray(cv2.cvtColor(region_img, cv2.COLOR_BGR2RGB))
@@ -20,7 +20,7 @@ def show_points(points, object_to_detect=None, width=1024, scale=None):
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
 
-def get_points_img(points: List[Point], object_to_detect=None, width=2048, user_position=None, scale=None):
+def get_points_img(points: List[Point], width=2048, user_position=None, scale=None):
     if points is None or len(points) < 1: return
 
     point_start = points[0]
@@ -52,11 +52,13 @@ def get_points_img(points: List[Point], object_to_detect=None, width=2048, user_
         # cv2.arrowedLine(region_img, point_B, point_A, (255, 0, 0), 1, tipLength=0.2)
         last_point = point
 
+        if index == 0: # 起点设置为红色
+            color = (0, 0, 255)  # 绿色，BGR格式
         # 画点（在指定坐标处绘制一个红色的点，大小为2）
-        if point.type == object_to_detect:
+        elif point.type == point.TYPE_TARGET:
             color = (0, 255, 0)  # 绿色，BGR格式
         elif point.type == Point.TYPE_PATH:
-            color = (0, 255, 255)  # 黄色，BGR格式
+            color = (255, 0, 0)  # 蓝色，BGR格式
         else:
             color = (100, 100, 100)
         thickness = 1
@@ -95,7 +97,7 @@ def get_points_img_live(points: [Point], object_to_detect=None, width=1024,scale
     if points is None or len(points) == 0:
         return None
     pos = MinimapInterface.get_position()
-    region_img = get_points_img(points, object_to_detect, width, pos, scale)
+    region_img = get_points_img(points, width, pos, scale)
     if pos is not None: cv2.circle(region_img, (width//2,width//2), 5, (0,255,255), 2)
     return region_img
 
@@ -123,18 +125,19 @@ def show_points_live(points: List[Point], object_to_detect=None, width=1024,scal
         sys.exit(0)
 
 
-
 if __name__ == '__main__':
     # jsonfile = getjson_path_byname('甜甜花_蒙德_清泉镇_2024-08-08_15_42_55.json')
-    jsonfile = getjson_path_byname('搜刮_蒙德_追星山谷_2024-04-30_11_42_02.json')
+    # jsonfile = getjson_path_byname('调查_璃月_地中之岩_2024-04-29_06_23_28.json')
+    # jsonfile = getjson_path_byname('月莲_须弥_降魔山下_6个.json')
+    jsonfile = getjson_path_byname('月莲_test.json')
     # jsonfile = getjson('jiuguan_蒙德_test_scale2_20240808.json')
     # jsonfile = getjson_path_byname('jiuguan_蒙德_wfsd_20240808.json')
     # jsonfile = getjson_path_byname('jiuguan_枫丹_tiantianhua_20240808.json')
     # jsonfile = getjson_path_byname('甜甜花_枫丹_中央实验室遗址_test_2024-08-08_12_37_05.json')
     json_map = BasePathExecutor.load_json(jsonfile)
-    show_points(json_map['positions'], json_map['name'], 1024, scale=1)
+    show_points(json_map['positions'],  1024, scale=0.4)
     # show_points(json_map.get('positions'), json_map['name'],width=1200,scale=3)
     # while True:
-    #     time.sleep(0.1)
-        # show_points_live(json_map.get('positions'), json_map['name'],scale=1.5)
+        # time.sleep(0.1)
+        # show_points_live(json_map.get('positions'), scale=1.5)
     #     show_points_live(path_list, object_to_detect=object_to_detect, width=1024)
