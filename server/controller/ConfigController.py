8af5f7b330a5a030d4134d2e31010e86e6d55c0f
tignioj <859897994@qq.com>
2024-09-02 00:19:06 +0800

    统一成功/失败返回

--- server/controller/ConfigController.py
+++ server/controller/ConfigController.py
@@ -1,17 +1,18 @@
 import os
 
 from flask import Blueprint, jsonify, request
-
+from server.controller.ServerBaseController import ServerBaseController
 config_bp = Blueprint('config_bp', __name__)
 
-class ConfigController:
+class ConfigController(ServerBaseController):
     @staticmethod
     @config_bp.route('/config/get')
     def config_get():
         from myutils.configutils import application_path, config_name
         config_path = os.path.join(application_path, config_name)
         with open(config_path, "r", encoding="utf8") as f:
-            return jsonify({'success': True, 'data': f.read()})
+            # return jsonify({'success': True, 'data': f.read()})
+            return ConfigController.success(data=f.read())
 
     # @app.route('/config/edit')
     # def config_editor():
@@ -31,5 +32,5 @@ class ConfigController:
         with open(config_path, "w", encoding="utf8") as f:
             f.write(request.get_data(as_text=True))
         reload_config()
-        return jsonify({'success': True, 'msg': '保存配置成功'})
+        return ConfigController.success(message='保存配置成功')
 
