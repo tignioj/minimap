7024952d253d0170a5061acddb8a1ede5f3bdec7
tignioj <859897994@qq.com>
2024-09-03 08:31:12 +0800

    统一返回接口

--- server/controller/TodoController.py
+++ server/controller/TodoController.py
@@ -2,19 +2,21 @@ from flask import Blueprint, jsonify, request
 from threading import Thread
 import os
 
+from server.controller.ServerBaseController import ServerBaseController
+
 # 创建一个蓝图
 todo_bp = Blueprint('todo', __name__)
 from server.service.TodoService import TodoService, TodoExecuteException
 
-class TodoController:
+class TodoController(ServerBaseController):
     @staticmethod
     @todo_bp.route('/todo/get')
     def todo_get():
         try:
             data = TodoService.get_all_todos()
-            return jsonify({'success': True, 'data': data})
-        except:
-            return jsonify({'success': False, 'data': data})
+            return TodoController.success(data=data)
+        except Exception as e:
+            return TodoController.error(message=e.args)
 
     @staticmethod
     @todo_bp.post('/todo/save')
@@ -25,20 +27,12 @@ class TodoController:
             return TodoController.error('保存失败')
 
     @staticmethod
-    def success(status=None, message=None):
-        return jsonify({'success': True, 'status': status, 'message': message})
-
-    @staticmethod
-    def error(status=None, message=None):
-        return jsonify({'success': False, 'status': status, 'message': message})
-
-    @staticmethod
     @todo_bp.post('/todo/run')
     def todo_run():
         todo_json = request.get_json()
         try:
             TodoService.todo_run(todo_json)
-            return TodoController.success(msg='成功执行')
+            return TodoController.success(message='成功执行')
         except TodoExecuteException as e:
             return TodoController.error(e.args)
 
@@ -47,7 +41,7 @@ class TodoController:
     def todo_stop():
         try:
             TodoService.todo_stop()
-            return TodoController.success(msg='停止执行清单')
+            return TodoController.success(message='停止执行清单')
         except TodoExecuteException as e:
-            return TodoController.error(msg=e.args)
+            return TodoController.error(message=e.args)
 
