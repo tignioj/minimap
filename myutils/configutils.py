0bddabe6c2b25c31c8048f5a6657b2bcec65f999
tignioj <859897994@qq.com>
2024-09-05 23:46:07 +0800

    ruamel.yaml回写不会删掉注释而PyYAML保存会删掉所有注释，因此替换掉了PyYAML库

--- myutils/configutils.py
+++ myutils/configutils.py
@@ -1,4 +1,8 @@
-import yaml, os
+import os
+# 创建 YAML 对象，保留注释
+from ruamel.yaml import YAML
+yaml = YAML()
+yaml.preserve_quotes = True
 
 import sys
 config_name = 'config.dev.yaml'
@@ -11,13 +15,19 @@ elif __file__:
     application_path = os.path.dirname(os.path.dirname(__file__))
 
 PROJECT_PATH = application_path
+
+YAML_KEY_DEFAULT_FIGHT_TEAM = 'default_fight_team'
+YAML_KEY_DEFAULT_FIGHT_DURATION = 'fight_duration'
+
+
 def load_config():
-    config_path = os.path.join(application_path, config_name)
+    config_path = get_config_file_path()
     with open(config_path, "r", encoding="utf8") as stream:
-        try:
-            return yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
+        return yaml.load(stream)
+
+def get_config_file_path():
+    return os.path.join(application_path, config_name)
+
 def reload_config():
     global cfg,resource_path
     cfg = load_config()
@@ -33,6 +43,20 @@ if cfg.get('resources_path') is not None:
     resource_path = cfg['resource_path']
 else:
     resource_path = os.path.join(PROJECT_PATH, 'resources')
+
+def set_config(key, value=None):
+    reload_config()
+    old_val = cfg[key]
+    try:
+        cfg[key] = value
+        config_path = get_config_file_path()
+        with open(config_path, "w", encoding="utf8") as f:
+            yaml.dump(cfg, f)
+    except Exception as e:
+        # 失败回滚
+        cfg[key] = old_val
+        raise e
+
 
 def get_bigmap_path(size=2048,version=5.0):
     # return os.path.join(resource_path, 'map', f'combined_image_{size}.png')
@@ -54,11 +78,7 @@ def get_user_folder():
     return user_folder
 
 if __name__ == '__main__':
-    path = get_bigmap_path()
-    print(path)
-    kp, des = get_keypoints_des_path()
-    print(kp, des)
-    p = get_paimon_icon_path()
-    print(p)
-    port = cfg.get('minimap').get('port')
-    print(port)
+    df = get_config('default_fight_team')
+    print(df)
+    set_config('default_fight_team', '1.txt')
+    print(get_config('default_fight_team'))
