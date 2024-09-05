8b249aa94bd33a926db5af5a98f5f34cf612d196
tignioj <859897994@qq.com>
2024-09-05 23:42:57 +0800

    提取出两种运行模式：1.从服务器中已经存储的文本中执行。2.从网络请求中执行

--- controller/FightController.py
+++ controller/FightController.py
@@ -1,13 +1,12 @@
 import os.path
 import re
-import sys
 import threading
 import time
-from array import array
-from random import random
 
 # 战斗
 from controller.BaseController import BaseController
+from fightmapper.FightMapperImpl import FightMapperImpl
+
 
 # e:2  e长按两秒
 # attack(10) 连续攻击10秒
@@ -18,42 +17,80 @@ class StopFightException(Exception): pass
 
 
 class FightController(BaseController):
-    def __init__(self, file_name='莱依拉_芙宁娜_枫原万叶_散兵(莱芙万散).txt'):
+
+    def __init__(self, filename, memory_mode=False):
         super().__init__()
+        self.memory_mode = memory_mode
+        self.fighting_thread = None
         self.characters_with_skills = []
-        self.team_name = ''
-        self.character_names = []
         self.current_character = None
-        self.read_txt(file_name)
         self.stop_fight = False
+        self.lastmod: float = None
+        if not filename:
+            from myutils.configutils import get_config, YAML_KEY_DEFAULT_FIGHT_TEAM
+            filename = get_config(YAML_KEY_DEFAULT_FIGHT_TEAM)
 
-    def read_txt(self, file_name):
+        self.filename = filename
+        self.team_name = None
+        self.characters_name = None
+        self.fight_mapper = FightMapperImpl(character_name=None)
+        self.load_characters_with_skills_from_file()
+
+    def get_teamname_from_string(self, file_name):
+        team_name = file_name[file_name.index("(") + 1:file_name.rindex(")")]
+        return team_name
+
+    def get_characters_from_string(self, file_name):
+        character_names = file_name[:file_name.index("(")].split("_")
+        return character_names
+
+    def load_data_from_text(self, text):
+        lines = text.split("\n")
+        for line in lines:
+            line = line.strip()
+            if line.startswith("//") or len(line) == 0:
+                continue
+            character_with_skills = {'name': None, 'skills': []}
+            # 角色
+            character = line[:line.index(" ")]
+            skill_str = line[line.index(" ") + 1:]
+            character_with_skills['name'] = character
+            sp = skill_str.split(",")
+            for key in sp:
+                skey = key.strip()
+                character_with_skills['skills'].append(skey)
+            self.characters_with_skills.append(character_with_skills)
+
+    def update_data(self, team_name, characters_name, text):
+        self.team_name = team_name
+        # 去掉空白字符串
+        self.characters_name = list(filter(None, characters_name))
+        self.load_data_from_text(text)
+    #
+    def load_characters_with_skills_from_memory(self, characters_name:[], text:str, team_name:str):
+        self.update_data(team_name=team_name, characters_name=characters_name, text=text)
+
+    def load_characters_with_skills_from_file(self):
+        # 如果是从内存中读取，则不需要读取文件
+        if self.memory_mode: return
+
+        filename = self.filename
         # 1. 读取队伍列表
-        from myutils.configutils import resource_path
-        team_folder = os.path.join(resource_path, 'user', 'team')
-        user_team = os.path.join(team_folder, file_name)
+        from myutils.configutils import get_user_folder
+        team_folder = os.path.join(get_user_folder(), 'team')
+        fight_file = os.path.join(team_folder, filename)
+        # 没改变不用重新读取
+        if self.lastmod == os.path.getmtime(fight_file):
+            return
+        self.lastmod = os.path.getmtime(fight_file)
+        # print(self.team_name, self.character_names)
+        with open(fight_file, 'r', encoding='utf8') as f:
+            text = f.read()
 
-        self.team_name = file_name[file_name.index("(")+1:file_name.rindex(")")]
-        self.character_names = file_name[:file_name.index("(")].split("_")
-        print(self.team_name, self.character_names)
+        teamname = self.get_teamname_from_string(filename)
+        characters = self.get_characters_from_string(filename)
+        self.update_data(team_name=teamname, characters_name=characters, text=text)
 
-        with open(user_team, 'r', encoding='utf8') as f:
-            line = f.readline()
-            while line:
-                if line.strip().startswith("//"):
-                    line = f.readline()
-                    continue
-                character_with_skills = {'name': None, 'skills': []}
-                # 角色
-                character = line[:line.index(" ")]
-                skill_str = line[line.index(" ")+1:]
-                character_with_skills['name'] = character
-                sp = skill_str.split(",")
-                for key in sp:
-                    skey = key.strip()
-                    character_with_skills['skills'].append(skey)
-                self.characters_with_skills.append(character_with_skills)
-                line = f.readline()
     def switch_character(self, character_name, wait_time=8):
         """
         切角色。
@@ -62,11 +99,14 @@ class FightController(BaseController):
         :return:
         """
         character_number = self.get_character_number(character_name)
+        success = True
 
         start_time = time.time()
         from random import randint
         while self.gc.get_team_current_number() != character_number:
-            if time.time() - start_time > wait_time: break
+            if time.time() - start_time > wait_time:
+                success = False
+                break
             # if self.stop_fight: raise StopFightException()
             # 稍微动一下屏幕让模板匹配更容易成功
             x = randint(-100, 100)
@@ -79,12 +119,19 @@ class FightController(BaseController):
 
             time.sleep(0.1)
             self.kb_press_and_release(str(character_number))
-        self.logger.debug(f"切人{character_number}成功")
 
-        return True
+        # self.logger.debug(f"切人{character_number} {'成功' if success else '失败'}")
+        if success:
+            self.logger.debug(f"切人{character_number}成功")
+        else:
+            self.logger.warning(f"切人{character_number}超时失败")
+
+        return success
 
     def get_character_number(self, name):
-        return self.character_names.index(name) + 1
+        if name not in self.characters_name:
+            raise StopFightException(f"你指定的角色{name}不在队伍${self.characters_name}中")
+        return self.characters_name.index(name) + 1
 
     def character_fight(self, character_with_skills):
         # 切人
@@ -95,12 +142,12 @@ class FightController(BaseController):
         print(f'character{character}, num {character_number}, skills {skills}')
         while not self.switch_character(character): time.sleep(0.1)
         self.current_character = character
+        self.fight_mapper.character_name = character
         for skill in skills:
             time.sleep(0.1)
-        #     TODO: 检验键盘值是否在允许字符内避免恶意按键
             self.do_skill(skill)
 
-    def parse_method_call(self,method_call_str):
+    def parse_method_call(self, method_call_str):
         # 使用正则表达式提取方法名称和参数
         pattern = r'(\w+)\((.*)\)'
         match = re.match(pattern, method_call_str)
@@ -118,13 +165,11 @@ class FightController(BaseController):
         return method_name, params
 
     def do_skill(self, skill):
-        from fightmapper.BaseFightMapper import BaseFightMapper
-        fight_mapper = BaseFightMapper()
         method_name, params = self.parse_method_call(skill)
         if self.stop_fight: raise StopFightException()
         # 使用反射调用方法
-        if hasattr(fight_mapper, method_name):
-            method = getattr(fight_mapper, method_name)
+        if hasattr(self.fight_mapper, method_name):
+            method = getattr(self.fight_mapper, method_name)
             if callable(method):
                 method(*params)
             else:
@@ -134,27 +179,78 @@ class FightController(BaseController):
 
     def execute(self):
         for character_with_skill in self.characters_with_skills:
+            if self.stop_fight: raise StopFightException()
             self.character_fight(character_with_skill)
 
     def execute_infinity(self):
         try:
-            self.execute()
+            while not self.stop_fight:
+                self.execute()
         except StopFightException as e:
-            raise e
+            self.logger.debug(e.args)
         finally:
             # 打断所有动作， 恢复状态
             if self.current_character == '散兵':
                 # 连续按2次e避免还在空中
                 self.kb_press_and_release('e')
-                time.sleep(0.05)
+                time.sleep(0.6)
                 self.kb_press_and_release('e')
+                time.sleep(0.1)
 
             # 跳跃打断所有动作
             self.kb_press_and_release(self.Key.space)
+            self.current_character = None
+            self.team_name = None
+
+    def start_fighting(self):
+        self.stop_fight = False
+        if self.fighting_thread:
+            raise StopFightException("已经有线程正在执行，请先停止！")
+            # try:
+            #     self.stop_fighting()
+            # except Exception as e:
+            #     self.logger.debug(e)
+        if self.memory_mode:
+            # 如果是内存模式，检查数据是否成功加载
+            if len(self.characters_name) == 0:
+                raise StopFightException("正处于内存模式运行，但是未检测到队伍信息")
+        else:
+            self.load_characters_with_skills_from_file()
+
+        self.fighting_thread = threading.Thread(target=self.execute_infinity)
+        self.fighting_thread.start()
+
+    def stop_fighting(self):
+        self.stop_fight = True
+        if self.fighting_thread:
+            self.fighting_thread.join()
+            self.fighting_thread = None
 
 
 if __name__ == '__main__':
-    f = FightController()
-    threading.Thread(target=f.execute_infinity).start()
-    time.sleep(10)
-    f.stop_fight = True
+    from myutils.configutils import get_user_folder
+
+    from pynput.keyboard import Listener, Key
+
+    # file_name = '那维莱特_莱伊拉_迪希雅_行秋(龙莱迪行).txt'
+    # file_name = '那维莱特_莱伊拉_行秋_枫原万叶(龙莱行万).txt'
+    file_name = '莱依拉_芙宁娜_枫原万叶_流浪者_(莱芙万流).txt'
+    fc = FightController(file_name)
+    # def _on_press(key):
+    #     try:
+    #         if key.char == '`':
+    #             if fc.stop_fight:
+    #                 fc.start_fighting()
+    #             else:
+    #                 fc.stop_fighting()
+    #     except AttributeError as e:
+    #         pass
+    #
+    # l = Listener(on_press=_on_press)
+    # l.start()
+    # l.join()
+
+    # time.sleep(12)
+    # print('等待结束中', time.time())
+    # fc.stop_fighting()
+    # print('结束了', time.time())
