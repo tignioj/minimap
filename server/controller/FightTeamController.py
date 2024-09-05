026372ba87f938c9b16fc4d5ff08917db2a58eee
tignioj <859897994@qq.com>
2024-09-05 14:09:45 +0800

    战斗队伍编辑

---
+++ server/controller/FightTeamController.py
@@ -0,0 +1,82 @@
+import json
+import os
+
+from flask import Blueprint, jsonify, request, render_template
+
+from server.controller.ServerBaseController import ServerBaseController
+
+fight_team_bp = Blueprint('fight_team', __name__)
+
+from server.service.FightTeamService import FightTeamService, FightTeamServiceException
+fightteam_service = FightTeamService()
+class FightTeamController(ServerBaseController):
+
+    @staticmethod
+    @fight_team_bp.get('/fight_team/list')
+    def get_fight_team_list():
+        team_list = fightteam_service.list_teams()
+        return FightTeamController.success(data=team_list)
+
+    @staticmethod
+    @fight_team_bp.post('/fight_team/create/<team_name>')
+    def create_fightteam(team_name):
+        plain_text = request.get_data(as_text=True)
+        try:
+            result = fightteam_service.create_team(team_name, plain_text)
+            return FightTeamController.success(message=result)
+        except FightTeamServiceException as e:
+            return FightTeamController.error(message=str(e.args))
+
+    @staticmethod
+    @fight_team_bp.delete('/fight_team/delete/<team_file_name>')
+    def delete_fightteam(team_file_name):
+        try:
+            result = fightteam_service.delete_team(team_file_name)
+            return FightTeamController.success(message=result)
+        except FightTeamServiceException as e:
+            return FightTeamController.error(message=str(e.args))
+
+    @staticmethod
+    @fight_team_bp.put('/fight_team/update/<team_file_name>')
+    def update_fightteam(team_file_name):
+        data = request.get_data(as_text=True)
+        new_team_name = request.args.get('new_team_name')
+        try:
+            result = fightteam_service.update_team(team_file_name, new_team_name, data)
+            return FightTeamController.success(message=result)
+        except (FileNotFoundError, FightTeamServiceException) as e:
+            return FightTeamController.error(message=e.args)
+
+    @staticmethod
+    @fight_team_bp.get('/fight_team/get/<filename>')
+    def get_fightteam(filename):
+        try:
+            content = fightteam_service.get_team(filename)
+            return FightTeamController.success(data=content)
+        except (FileNotFoundError, FightTeamServiceException) as e:
+            return FightTeamController.error(message=e.args)
+
+
+    @staticmethod
+    @fight_team_bp.get('/fight_team/run/<filename>')
+    def get_run_fightteam(filename):
+        try:
+            return FightTeamController.success(fightteam_service.run_teams(filename))
+        except (FileNotFoundError, FightTeamServiceException) as e:
+            return FightTeamController.error(message=e.args)
+    @staticmethod
+    @fight_team_bp.get('/fight_team/stop')
+    def get_stop_fightteam():
+        try:
+            return FightTeamController.success(fightteam_service.stop_fighting())
+        except (FileNotFoundError, FightTeamServiceException) as e:
+            return FightTeamController.error(message=e.args)
+
+    @staticmethod
+    @fight_team_bp.put('/fight_team/set_default/<filename>')
+    def set_default_fightteam(filename):
+        try:
+            return FightTeamController.success(fightteam_service.set_default(filename))
+        except (FileNotFoundError, FightTeamServiceException) as e:
+            return FightTeamController.error(message=e.args)
+
---
+++ server/service/FightTeamService.py
@@ -0,0 +1,139 @@
+import os
+
+from myutils.configutils import get_user_folder
+
+class FightTeamServiceException(Exception): pass
+from controller.FightController import FightController
+from mylogger.MyLogger3 import MyLogger
+logger = MyLogger('fight_team_service')
+
+class FightTeamService:
+
+    def __init__(self):
+        user_folder = get_user_folder()
+        self.team_folder_path = os.path.join(user_folder, 'team')
+        self.fight_controller:FightController = None
+
+    def check_team_content_valid(self, team_file_name, content):
+        # 检查内容中选择的角色是否在文件名中给出，没有则抛出异常
+        team_str = team_file_name[:team_file_name.find('(')-1]
+        team_split = team_str.split("_")
+        # 去掉空字符串
+        team_split = [item for item in team_split if item]
+        if len(team_split) != 4:
+            raise FightTeamServiceException("队伍长度必须是4")
+
+        lines = content.split("\n")
+        # 检查每一行第一列角色是否在team list中
+        error_character = []
+        for line in lines:
+            line:str = line.strip()
+            if len(line) == 0: continue
+            if line.startswith("//"): continue
+
+            # 提取角色名称
+            character_name = line[:line.find(" ")+1].strip()
+            if character_name not in team_split:
+                # 不存重复的
+                if character_name not in error_character:
+                    error_character.append(character_name)
+        if len(error_character) > 0:
+            raise FightTeamServiceException(f"请检查{str(error_character)}角色是否包含在文件名中!")
+
+        return True
+
+    def create_team(self, team_file_name, content):
+        team_file_path = os.path.join(self.team_folder_path, team_file_name)
+        if os.path.exists(team_file_path):
+            raise FightTeamServiceException("已存在同命名文件队伍，创建失败!")
+        self.check_team_content_valid(team_file_name, content)
+        with open(team_file_path, 'w', encoding='utf8') as f:
+            f.write(content)
+        return f"成功创建队伍{team_file_name}"
+
+    def __get_team_file_path(self, team_file_name):
+        team_file_path = os.path.join(self.team_folder_path, team_file_name)
+        if not os.path.exists(team_file_path):
+            raise FightTeamServiceException(f'你要查找的{team_file_name}文件未找到')
+        return team_file_path
+    def get_team(self, team_file_name):
+        team_file_path = self.__get_team_file_path(team_file_name)
+        with open(team_file_path, 'r', encoding='utf8') as f:
+            return f.read()
+
+    def update_team(self, team_file_name, new_team_name, data):
+        team_file_path = self.__get_team_file_path(team_file_name)
+
+        self.check_team_content_valid(new_team_name, data)
+        with open(team_file_path, 'w', encoding='utf8') as f:
+            f.write(data)
+
+        new_name = os.path.join(get_user_folder(), 'team', new_team_name)
+
+        # 如果名称没改变，直接返回更新成功
+        if new_team_name == team_file_name:
+            return f"成功更新{team_file_name}"
+
+        # 如果名称改变了，但是新的名称已经存在，则抛出异常
+        if os.path.exists(new_name) and new_team_name != team_file_name:
+            raise FightTeamServiceException("文件内容更新成功，但是已经存在同名文件，改名失败")
+
+        # 其余情况就是名称改变了,且新的文件名可用，则返回成功
+        try:
+            os.rename(team_file_path, new_name)
+            return f"成功更新{team_file_name},并且更名为{new_team_name}"
+        except Exception as e:
+            logger.exception(e)
+            raise FightTeamServiceException(f"文件内容已经更新，但是重命名失败{str(e.args)}")
+
+
+    def delete_team(self, team_file_name=None):
+        team_file_path = self.__get_team_file_path(team_file_name)
+        try:
+            os.remove(team_file_path)
+            return f"删除{team_file_name}成功"
+        except Exception as e:
+            logger.exception(e)
+            return f"删除{team_file_name}失败{str(e.args)}"
+
+    def list_teams(self):
+        default_team = self.get_default()
+        files = os.listdir(self.team_folder_path)
+        return {'default': default_team, 'files': files}
+
+    def run_teams(self, filename):
+        if self.fight_controller:
+            try: self.fight_controller.stop_fighting()
+            except:pass
+        self.__get_team_file_path(filename)
+        self.fight_controller = FightController(filename)
+        self.fight_controller.start_fighting()
+        return f"成功运行{filename}"
+
+    def stop_fighting(self):
+        if self.fight_controller:
+            try: self.fight_controller.stop_fighting()
+            except Exception as e:
+                logger.exception(e)
+                raise FightTeamServiceException("你没有运行，无需停止")
+        self.fight_controller = None
+        return "成功停止"
+
+    def set_default(self, filename):
+        from myutils.configutils import set_config, YAML_KEY_DEFAULT_FIGHT_TEAM
+        try:
+            set_config(YAML_KEY_DEFAULT_FIGHT_TEAM, filename)
+            return f"设置默认战斗队伍为{filename}成功"
+        except Exception as e:
+            logger.exception(e,exc_info=True)
+            raise FightTeamServiceException(f"设置默认队伍失败:{e.args}")
+
+
+    def get_default(self):
+        from myutils.configutils import get_config, YAML_KEY_DEFAULT_FIGHT_TEAM
+        try:
+            return get_config(YAML_KEY_DEFAULT_FIGHT_TEAM)
+        except Exception as e:
+            logger.exception(e,exc_info=True)
+            raise FightTeamServiceException(f"设置默认队伍失败:{e.args}")
+
