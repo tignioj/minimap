e63be420248d18e8b537ef649ac59f32447bb652
tignioj <859897994@qq.com>
2024-09-11 05:40:31 +0800

    改名

--- myexecutor/LeyLineOutcropPathExecutor.py
+++ myexecutor/LeyLineOutcropPathExecutor.py
@@ -24,7 +24,7 @@ class ExecuteTimeOutException(Exception): pass
 class NoResinException(Exception): pass
 
 
-class DimaiPoint(Point):
+class LeyLineOutcropPoint(Point):
     ACTION_FIGHT = 'fight'  # 战斗地点
     ACTION_REWARD = 'reward'  # 领取奖励的点
 
@@ -32,32 +32,32 @@ class DimaiPoint(Point):
         super().__init__(x=x,y=y,type=type,move_mode=move_mode,action=action)
         self.events = events
 
-class DimaiPath(BasePath):
-    def __init__(self, name, country, positions: List[DimaiPoint], anchor_name=None, enable=None):
+class LeyLineOutcropPath(BasePath):
+    def __init__(self, name, country, positions: List[LeyLineOutcropPoint], anchor_name=None, enable=None):
         super().__init__(name=name, country=country, positions=positions, anchor_name=anchor_name)
         self.enable=enable
         self.note = None  # 自定义备注
         # TODO：指定委托所在的位置，用于搜索；对于战斗类型通常是最后一个点的位置，其他的不一定
         # self.mission_position = None  #
 
-class DimaiPathExecutor(BasePathExecutor):
+class LeyLineOutcropPathExecutor(BasePathExecutor):
     def __init__(self, json_file_path, debug_enable=None):
         super().__init__(json_file_path=json_file_path, debug_enable=debug_enable)
         self.fight_controller = FightController(None)
 
     @staticmethod
-    def load_basepath_from_json_file(json_file_path) -> DimaiPath:
+    def load_basepath_from_json_file(json_file_path) -> LeyLineOutcropPath:
         with open(json_file_path, encoding="utf-8") as r:
             json_dict = json.load(r)
-            points: List[DimaiPoint] = []
+            points: List[LeyLineOutcropPoint] = []
             for point in json_dict.get('positions', []):
-                p = DimaiPoint(x=point.get('x'),
+                p = LeyLineOutcropPoint(x=point.get('x'),
                           y=point.get('y'),
                           type=point.get('type', Point.TYPE_PATH),
                           move_mode=point.get('move_mode', Point.MOVE_MODE_NORMAL),
                           action=point.get('action'),events=point.get('events'))
                 points.append(p)
-            return DimaiPath(
+            return LeyLineOutcropPath(
                 name=json_dict['name'], country=json_dict['country'], positions=points,
                 anchor_name=json_dict['anchor_name'],
                 enable=json_dict.get('enable', True)  # 未记录完成的委托标记为False
@@ -165,16 +165,16 @@ class DimaiPathExecutor(BasePathExecutor):
             if time.time() - start_time > 30: raise MoveToLocationTimeoutException("移动超时！")
 
     @staticmethod
-    def get_closet_dimai(map_controller):
+    def get_closet_leyline_outcrop(map_controller):
         closet_missions = []
         # TODO: BUG: 缩放的值可能会随着地图版本更新而不同
         map_controller.scales_adjust(0.3, 0.33)
-        # 模板匹配屏幕中出现的委托,得到他们的屏幕坐标
-        missions_screen_points = DimaiPathExecutor.get_mission_template_matched_screen_position()
+        # 模板匹配屏幕中出现的地脉图标,得到他们的屏幕坐标
+        missions_screen_points = LeyLineOutcropPathExecutor.get_mission_template_matched_screen_position()
         # 计算得到世界坐标
         mission_world_points = map_controller.get_world_coordinate(missions_screen_points)
         for mission_world_point in mission_world_points:
-            closest:str = DimaiPathExecutor.get_specify_point_closest_mission_json(mission_world_point)
+            closest:str = LeyLineOutcropPathExecutor.get_specify_point_closest_mission_json(mission_world_point)
             if closest is None: continue
             closet_missions.append(closest)
         return closet_missions
@@ -195,37 +195,37 @@ class DimaiPathExecutor(BasePathExecutor):
 
         map_controller.open_middle_map()  # 再次打开地图
         # 先查当前大地图有没有地脉，没有则回神像逐个检测
-        dimai_arr = DimaiPathExecutor.get_closet_dimai(map_controller)
-        if len(dimai_arr) == 0:
-            DimaiPathExecutor.go_to_seven_anemo(map_controller)
+        leyline_outcrop = LeyLineOutcropPathExecutor.get_closet_leyline_outcrop(map_controller)
+        if len(leyline_outcrop) == 0:
+            LeyLineOutcropPathExecutor.go_to_seven_anemo(map_controller)
             map_controller.open_middle_map()  # 再次打开地图
             time.sleep(1)
-        else: return dimai_arr
+        else: return leyline_outcrop
 
         # 查找最近的地脉
         # 这里是蒙德清泉镇地脉
-        dimai_arr = DimaiPathExecutor.get_closet_dimai(map_controller)
+        leyline_outcrop = LeyLineOutcropPathExecutor.get_closet_leyline_outcrop(map_controller)
 
-        if len(dimai_arr) == 0:
+        if len(leyline_outcrop) == 0:
             # 星落湖七天神像查找
             x, y = 3451.571140, -6365.88
-            DimaiPathExecutor.move_map_to(map_controller, (x,y), time.time())
-            dimai_arr = DimaiPathExecutor.get_closet_dimai(map_controller)
+            LeyLineOutcropPathExecutor.move_map_to(map_controller, (x,y), time.time())
+            leyline_outcrop = LeyLineOutcropPathExecutor.get_closet_leyline_outcrop(map_controller)
 
 
-        if len(dimai_arr) == 0:
+        if len(leyline_outcrop) == 0:
             # 风龙废墟七天神像查找
             x, y = 436.84067, -6526.89
-            DimaiPathExecutor.move_map_to(map_controller, (x,y), time.time())
-            dimai_arr = DimaiPathExecutor.get_closet_dimai(map_controller)
+            LeyLineOutcropPathExecutor.move_map_to(map_controller, (x,y), time.time())
+            leyline_outcrop = LeyLineOutcropPathExecutor.get_closet_leyline_outcrop(map_controller)
 
-        if len(dimai_arr) == 0:
+        if len(leyline_outcrop) == 0:
             # 雪山七天神像查找
             x, y = 2840.573, -3591.75
-            DimaiPathExecutor.move_map_to(map_controller, (x,y), time.time())
-            dimai_arr = DimaiPathExecutor.get_closet_dimai(map_controller)
+            LeyLineOutcropPathExecutor.move_map_to(map_controller, (x,y), time.time())
+            leyline_outcrop = LeyLineOutcropPathExecutor.get_closet_leyline_outcrop(map_controller)
 
-        return dimai_arr
+        return leyline_outcrop
 
 
     @staticmethod
@@ -238,7 +238,7 @@ class DimaiPathExecutor(BasePathExecutor):
     def execute_all_mission(emit=lambda val1,val2:None):  # 传一个空实现的方法，免去判断函数是否为空
         from server.service.DailyMissionService import SOCKET_EVENT_DAILY_MISSION_UPDATE, SOCKET_EVENT_DAILY_MISSION_END
         from controller.MapController2 import MapController
-        daily_task_execute_timeout:int = get_config('dimai_task_execute_timeout', 500)
+        daily_task_execute_timeout:int = get_config('layline_outcrop_task_execute_timeout', 500)
         if daily_task_execute_timeout < 60: daily_task_execute_timeout = 60
         elif daily_task_execute_timeout > 1200: daily_task_execute_timeout = 1200
 
@@ -248,7 +248,7 @@ class DimaiPathExecutor(BasePathExecutor):
 
         start_time = time.time()
         try:
-            closet_missions = DimaiPathExecutor.get_screen_world_mission_json(map_controller)
+            closet_missions = LeyLineOutcropPathExecutor.get_screen_world_mission_json(map_controller)
             while len(closet_missions) > 0:  # 不断执行委托直到屏幕上查找到的战斗委托为空
                 msg = f"查找到地脉:{closet_missions}"
                 logger.debug(msg)
@@ -258,8 +258,8 @@ class DimaiPathExecutor(BasePathExecutor):
                     msg = f"开始执行地脉任务:{closest}"
                     logger.debug(msg)
                     emit(SOCKET_EVENT_DAILY_MISSION_UPDATE, msg)
-                    DimaiPathExecutor(closest).execute()  # 可能会抛出体力耗尽异常
-                closet_missions = DimaiPathExecutor.get_screen_world_mission_json(map_controller)
+                    LeyLineOutcropPathExecutor(closest).execute()  # 可能会抛出体力耗尽异常
+                closet_missions = LeyLineOutcropPathExecutor.get_screen_world_mission_json(map_controller)
 
         except MoveToLocationTimeoutException as e:
             logger.error('移动地图超时！')
@@ -288,7 +288,7 @@ class DimaiPathExecutor(BasePathExecutor):
         self.fight_controller.stop_fighting()
 
     def wait_until_fight_finished(self):
-        daily_task_fight_timeout = get_config('dimai_task_fight_timeout', 20)
+        daily_task_fight_timeout = get_config('leyline_outcrop_task_fight_timeout', 20)
         if daily_task_fight_timeout < 10: daily_task_fight_timeout = 10
         elif daily_task_fight_timeout > 400: daily_task_fight_timeout = 400
 
@@ -308,12 +308,12 @@ class DimaiPathExecutor(BasePathExecutor):
         self.stop_fight()
 
     def on_nearby(self, coordinate):
-        if self.next_point.type == DimaiPoint.TYPE_TARGET:
-            if self.next_point.action == DimaiPoint.ACTION_FIGHT:
+        if self.next_point.type == LeyLineOutcropPoint.TYPE_TARGET:
+            if self.next_point.action == LeyLineOutcropPoint.ACTION_FIGHT:
                 if self.gc.has_gear():
                     self.log("发现齿轮")
                     self.crazy_f()
-            elif self.next_point.action == DimaiPoint.ACTION_REWARD:
+            elif self.next_point.action == LeyLineOutcropPoint.ACTION_REWARD:
                 # 怪物掉落材料太多可能会遮挡钥匙图标, 所以还不如直接判断reward图标
                 if self.gc.has_key() or self.gc.has_reward():
                     self.log("发现钥匙或者奖励图标")
@@ -334,9 +334,9 @@ class DimaiPathExecutor(BasePathExecutor):
                                 self.kb_press_and_release(self.Key.esc)  # 关闭对话框
                                 raise NoResinException("没有树脂了，结束地脉")
 
-    def on_move_after(self, point: DimaiPoint):
-        if point.type == DimaiPoint.TYPE_TARGET:
-            if point.action == DimaiPoint.ACTION_FIGHT:
+    def on_move_after(self, point: LeyLineOutcropPoint):
+        if point.type == LeyLineOutcropPoint.TYPE_TARGET:
+            if point.action == LeyLineOutcropPoint.ACTION_FIGHT:
                 self.crazy_f()
                 # 开启地脉
                 # OCR检测文字：点击浓缩树脂
@@ -344,7 +344,7 @@ class DimaiPathExecutor(BasePathExecutor):
                 if not self.gc.has_reward() and not self.gc.has_key():
                     self.wait_until_fight_finished()
                     self.logger.debug("战斗结束")
-            elif point.action == DimaiPoint.ACTION_REWARD:
+            elif point.action == LeyLineOutcropPoint.ACTION_REWARD:
                 self.logger.debug("到达领取奖励的地点")
                 # 前往领取奖励的点(地脉开启位置未必和领取奖励的位置相同)
                 # 只拾取一次可能只拿到了怪物的掉落材料而无法领取奖励
@@ -382,5 +382,5 @@ if __name__ == '__main__':
     # t = threading.Thread(target=BGIEventHandler.start_server)
     # t.setDaemon(True)
     # t.start()
-    DimaiPathExecutor.execute_all_mission()
+    LeyLineOutcropPathExecutor.execute_all_mission()
 
