c97bd220157b6ed9a2f41d455ce6d7bbdfa9f232
tignioj <859897994@qq.com>
2024-09-10 13:54:41 +0800

    ocr结果封装成对象

--- controller/OCRController.py
+++ controller/OCRController.py
@@ -1,4 +1,6 @@
 import time
+from typing import List
+
 from capture.capture_factory import capture
 from controller.BaseController import BaseController
 from matchmap.minimap_interface import MinimapInterface
@@ -7,20 +9,30 @@ from matchmap.minimap_interface import MinimapInterface
 # def get_ocr_result():
 #     return ocr_obj.ocr(gc.get_screenshot())
 
+class OCRResult:
+    def __init__(self, corner,text,percent):
+        self.corner = corner
+        self.text = text
+        self.percent =  percent
+    def __str__(self):
+        return str(self.text)
+
+
+
 # @cache
 class OCRController(BaseController):
     def __init__(self, debug_enable=False):
         self.debug_enable = debug_enable
         self.gc = capture
         super().__init__(debug_enable, self.gc)
-        self.ocr_result = None
+        self.ocr_result:List[OCRResult] = []
+        from myutils.timerutils import RateLimiter
+        self.ocr_update_limiter = RateLimiter(0.2)
+        self.ocr_update_limiter.execute(self.update_ocr_result)
 
-        self.update_timer = 0
-        self.UPDATE_TIME_SLEEP_TIME = 0.2  # 0.2秒刷新一次
-        self.update_ocr_result()
+    def get_ocr_result(self)->List[OCRResult]:
+        self.ocr_update_limiter.execute(self.update_ocr_result)
 
-    def get_ocr_result(self):
-        self.update_ocr_result()
         return self.ocr_result
 
     def update_ocr_result(self):
@@ -28,15 +40,26 @@ class OCRController(BaseController):
         OCR是一比巨大的开销，添加时间间隔防止调用卡顿
         :return:
         """
-        if time.time() - self.update_timer > self.UPDATE_TIME_SLEEP_TIME:
-            self.ocr_result = MinimapInterface.get_ocr_result()
-            self.update_timer = time.time()
+        result = MinimapInterface.get_ocr_result()
+        if not result: return
+
+        result = result[0]
+        if not result: return
+        self.ocr_result = []
+        for item in result:
+            ocr_result_obj = OCRResult(corner=item[0], text=item[1][0], percent=item[1][1])
+            self.ocr_result.append(ocr_result_obj)
+
 
     def click(self, x, y):
         pos = self.gc.get_screen_position((x, y))
         self.log((x, y), "->", pos)
         self.set_ms_position(pos)
         self.mouse_left_click()
+
+    def click_ocr_result(self, ocr_item:OCRResult):
+        center = self.get_line_center(ocr_item.corner)
+        self.click(center[0], center[1])
 
     def get_line_center(self, rect):
         """
@@ -54,24 +77,18 @@ class OCRController(BaseController):
         center_y = (left_top[1] + left_bottom[1]) / 2
         return center_x, center_y
 
-    def find_match_text(self, target_text, match_all=False):
-        if self.stop_listen or target_text is None: return
+    def find_match_text(self, target_text, match_all=False)->List[OCRResult]:
+        if self.stop_listen or target_text is None: return []
         self.log(f"正在查找'{target_text}'")
         # img = self.gc.get_screenshot()
         result = self.get_ocr_result()
-        match_texts = []
-        try:
-            if result is not None:
-                for idx in range(len(result)):
-                    res = result[idx]
-                    if res is None: continue
-                    for line in res:  # NoneType object is not iterable
-                        if target_text in line[1][0]:
-                            if match_all and target_text != line[1][0]: continue
-                            match_texts.append(line)
-        except Exception as e:
-            self.logger.error(e)
+        match_texts:List[OCRResult] = []
+        for item in result:
+            if target_text in item.text:
+                if match_all and target_text != item.text: continue
+                match_texts.append(item)
         return match_texts
+
     def find_text_and_click(self, target_text, match_all=False, index=0, click_all=False):
         """
         找到屏幕中匹配的文字并点击
@@ -82,8 +99,6 @@ class OCRController(BaseController):
         :return:
         """
         match_texts = self.find_match_text(target_text, match_all)
-
-        if match_texts is None: return False
 
         l = len(match_texts)
         if index < 0 or l == 0: return False
@@ -96,13 +111,13 @@ class OCRController(BaseController):
         if click_all:
             self.log(f"点击所有匹配的文本")
             for match in match_texts:
-                x, y = self.get_line_center(match[0])
+                x, y = self.get_line_center(match.corner)
                 self.click(x, y)
                 self.log(f"已经执行点击'{target_text}'操作")
         else:
             self.log(f"点击第{index + 1}个匹配的文本")
             match = match_texts[index]
-            x, y = self.get_line_center(match[0])
+            x, y = self.get_line_center(match.corner)
             self.click(x, y)
             self.log(f"已经执行点击'{target_text}'操作")
         return True
@@ -112,21 +127,27 @@ class OCRController(BaseController):
         if self.stop_listen or args is None: return
         self.log(f"在屏幕中查找{args}")
         result = self.get_ocr_result()
-        if result is None: return False
-        for idx in range(len(result)):
-            res = result[idx]
-            for line in res:
-                # if target_text in line[1][0]:
-                for arg in args:
-                    try:
-                        if arg is None: return False
-                        if arg in line[1][0]:
-                            self.log("屏幕上出现文本{}, 原文是{}:".format(arg, line[1][0]))
-                            return True
-                    except TypeError as e:
-                        self.log(line, e)
-                        return False
-        self.log(f"没有找到{args}")
+        for item in result:
+            for arg in args:
+                if arg in item.text:
+                    self.logger.debug(f"屏幕上出现文本{arg},原文是{item.text}")
+                    return True
+
+        # if result is None: return False
+        # for idx in range(len(result)):
+        #     res = result[idx]
+        #     for line in res:
+        #         # if target_text in line[1][0]:
+        #         for arg in args:
+        #             try:
+        #                 if arg is None: return False
+        #                 if arg in line[1][0]:
+        #                     self.log("屏幕上出现文本{}, 原文是{}:".format(arg, line[1][0]))
+        #                     return True
+        #             except TypeError as e:
+        #                 self.log(line, e)
+        #                 return False
+        # self.log(f"没有找到{args}")
         return False
 
 
@@ -145,10 +166,15 @@ if __name__ == '__main__':
     # print(ocr.find_text_and_click("传送锚点"))
     # print(ocr.is_text_in_screen("蔷薇"))
     while True:
-        start = time.time()
-        ocr.update_ocr_result()
-        result = ocr.is_text_in_screen("探索度")
-        # print(result)
-        print("cost{}".format(time.time() - start))
+        result = ocr.get_ocr_result()
+        print(result)
+        # time.sleep(15)
+        # ocr.is_text_in_screen('复苏')
+        # matches = ocr.find_match_text('复苏')
+        # for match in matches:
+        #     center = ocr.get_line_center(match.corner)
+        #     ocr.click(center[0], center[1])
+
+
 
 
