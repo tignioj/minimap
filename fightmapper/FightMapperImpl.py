f6c4d930429a8f21a7df36e873e6cb56d47e23bd
tignioj <859897994@qq.com>
2024-09-13 11:40:12 +0800

    修改晃动幅度

--- fightmapper/FightMapperImpl.py
+++ fightmapper/FightMapperImpl.py
@@ -24,30 +24,17 @@ class FightMapperImpl(BaseFightMapper):
         self.character_name = character_name
 
 
-    def __circle_loop(self, duration):
+    def __circle_loop(self, duration, updown=False):
         # 转圈
         t = time.time()
-        y = 1000
+        if updown: y = 1500
+        else: y = 0
         i = 0
         while time.time() - t < float(duration):
             i += 1
-            if i % 10 == 0: y = -y
+            if i % 5 == 0: y = -y
             self.camera_chage(dx=-500, dy=y)
             time.sleep(0.02)
-
-    # def up_down_grab_leaf(self):
-    #     time.sleep(0.5)
-    #     x, y = 0, -1000  # y代表垂直方向上的视角移动, x为水平方向
-    #     i = 40
-    #     # self.kb_press('w')  # 飞行
-    #     while i > 0 and not self.stop_listen:
-    #         if i % 10 == 0:
-    #             y = -y
-    #         i -= 1
-    #         self.logger.debug("上下晃动视角中")
-    #         win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, 0, y, 0, 0)
-    #         time.sleep(0.04)
-
 
     def charge(self, duration=None):
         """
@@ -57,7 +44,7 @@ class FightMapperImpl(BaseFightMapper):
         """
         t = None
         if self.character_name == '那维莱特':
-            t = threading.Thread(target=self.__circle_loop, args=(duration,))
+            t = threading.Thread(target=self.__circle_loop, args=(duration,True,))
             t.start()
         super().charge(duration=duration)
         if t: t.join()
@@ -70,7 +57,7 @@ class FightMapperImpl(BaseFightMapper):
         """
         t = None
         if self.character_name == '纳西妲' and hold:
-            t = threading.Thread(target=self.__circle_loop, args=(1,))
+            t = threading.Thread(target=self.__circle_loop, args=(1,False))
             t.start()
         super().skill(hold)
         if t: t.join()
@@ -78,11 +65,14 @@ class FightMapperImpl(BaseFightMapper):
 
 
 if __name__ == '__main__':
-    # fm = FightMapperImpl(character_name='那维莱特')
-    fm = FightMapperImpl(character_name='钟离')
-    fm.kb_press('w')
-    time.sleep(1)
-    fm.s(0.1)
-    # fm = FightMapperImpl(character_name='芙宁娜')
+    fm = FightMapperImpl(character_name='那维莱特')
+    # fm = FightMapperImpl(character_name='钟离')
+    # fm = FightMapperImpl(character_name='纳西妲')
     fm.e(hold=True)
-    # fm.charge(3)
+    fm.charge(3)
+    # fm.e()
+    fm.charge(3)
+    # fm.e()
+    fm.charge(3)
+    # fm.e()
+    fm.charge(3)
