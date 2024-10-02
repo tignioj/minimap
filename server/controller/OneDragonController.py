ed15205be3d228a14105ea7b47dffae1e7f9a081
tignioj <859897994@qq.com>
2024-10-03 05:35:38 +0800

    一条龙socket消息以及停止

--- server/controller/OneDragonController.py
+++ server/controller/OneDragonController.py
@@ -3,9 +3,18 @@ from flask import Blueprint, request, jsonify, send_file, current_app
 from capture.capture_factory import capture
 from server.controller.ServerBaseController import ServerBaseController
 from mylogger.MyLogger3 import MyLogger
+from server.service.OneDragonService import OneDragonException
+
 logger = MyLogger('one_dragon_controller')
 
 one_dragon_bp = Blueprint('one_dragon', __name__)
+
+
+ONE_DRAGON_START = 'socket_event_one_dragon_start'
+ONE_DRAGON_UPDATE = 'socket_event_one_dragon_update'
+ONE_DRAGON_END = 'socket_event_one_dragon_end'
+ONE_DRAGON_EXCEPTION = 'socket_event_one_dragon_exception'
+
 
 class OneDragonController(ServerBaseController):
     @staticmethod
@@ -15,7 +24,10 @@ class OneDragonController(ServerBaseController):
         logger.debug(one_dragon_list)
         from server.service.OneDragonService import OneDragonService
         socketio_instance = current_app.extensions['socketio']
-        OneDragonService.start_one_dragon(one_dragon_list, socketio_instance=socketio_instance)
+        try:
+            OneDragonService.start_one_dragon(one_dragon_list, socketio_instance=socketio_instance)
+        except OneDragonException as e:
+            return OneDragonController.error(e.args)
         return OneDragonController.success('开始运行一条龙')
 
     @staticmethod
--- server/service/OneDragonService.py
+++ server/service/OneDragonService.py
@@ -7,37 +7,73 @@ logger = MyLogger('one_dragon_service')
 #   { id: 3, name: '地脉', value: 'leyLine' , checked: false},
 #   { id: 4, name: '领取奖励', value: 'claimReward' , checked: false}
 # ]
+SOCKET_EVENT_ONE_DRAGON_START = 'socket_event_one_dragon_start'
+SOCKET_EVENT_ONE_DRAGON_UPDATE = 'socket_event_one_dragon_update'
+SOCKET_EVENT_ONE_DRAGON_END = 'socket_event_one_dragon_end'
+SOCKET_EVENT_ONE_DRAGON_EXCEPTION = 'socket_event_one_dragon_exception'
+
 from server.dto.DataClass import OneDragon
+class OneDragonException(Exception): pass
+import threading
+one_dragon_lock = threading.Lock()
 
 class OneDragonService:
+    one_dragon_thread = None
+
+    @staticmethod
+    def run(one_dragon_list, socketio_instance):
+        from controller.BaseController import BaseController
+        try:
+            for task in one_dragon_list:
+                if BaseController.stop_listen:
+                    logger.debug('停止监听')
+                    return
+
+                one_dragon = OneDragon.from_dict(task)
+                logger.info(one_dragon)
+                if not one_dragon.checked:
+                    continue
+                elif one_dragon.value == 'todo':
+                    from server.service.TodoService import TodoService
+                    # 传入空代表从已经保存的文件中执行
+                    TodoService.todo_run(todo_json=None, socketio_instance=socketio_instance)
+                    TodoService.todo_runner_thread.join()
+                elif one_dragon.value == 'dailyMission':
+                    from server.service.DailyMissionService import DailyMissionService
+                    DailyMissionService.start_daily_mission(socketio_instance=socketio_instance)
+                    DailyMissionService.daily_mission_thread.join()
+
+                elif one_dragon.value == 'leyLine':
+                    from server.service.LeyLineOutcropService import LeyLineOutcropService
+                    LeyLineOutcropService.start_leyline(leyline_type=None, socketio_instance=socketio_instance)
+                    LeyLineOutcropService.leyline_outcrop_thread.join()
+                elif one_dragon.value == 'claimReward':
+                    from server.service.DailyMissionService import DailyMissionService
+                    DailyMissionService.start_claim_reward(socketio_instance=socketio_instance)
+                    DailyMissionService.daily_mission_thread.join()
+
+        except Exception as e:
+            socketio_instance.emit(SOCKET_EVENT_ONE_DRAGON_EXCEPTION, str(e.args))
+        finally:
+            OneDragonService.one_dragon_thread = None
+            socketio_instance.emit(SOCKET_EVENT_ONE_DRAGON_END, "一条龙已停止")
+
     @staticmethod
     def start_one_dragon(one_dragon_list=None,socketio_instance=None):
-        for task in one_dragon_list:
-            one_dragon = OneDragon.from_dict(task)
-            logger.info(one_dragon)
-            if not one_dragon.checked: continue
-            elif one_dragon.value == 'todo':
-                from server.service.TodoService import TodoService
-                # 传入空代表从已经保存的文件中执行
-                TodoService.todo_run(todo_json=None,socketio_instance=socketio_instance)
-                TodoService.todo_runner_thread.join()
-            elif one_dragon.value == 'dailyMission':
-                from server.service.DailyMissionService import DailyMissionService
-                DailyMissionService.start_daily_mission(socketio_instance=socketio_instance)
-                DailyMissionService.daily_mission_thread.join()
+        if OneDragonService.one_dragon_thread is not None:
+            raise OneDragonException('一条龙已经在运行中，请勿重复执行')
+        with one_dragon_lock:
+            from controller.BaseController import BaseController
+            BaseController.stop_listen = False
+            OneDragonService.one_dragon_thread = threading.Thread(target=OneDragonService.run,
+                                                                  args=(one_dragon_list, socketio_instance,))
+            OneDragonService.one_dragon_thread.start()
 
-            elif one_dragon.value == 'leyLine':
-                from server.service.LeyLineOutcropService import LeyLineOutcropService
-                LeyLineOutcropService.start_leyline(leyline_type=None,socketio_instance=socketio_instance)
-                LeyLineOutcropService.leyline_outcrop_thread.join()
-            elif one_dragon.value == 'claimReward':
-                from server.service.DailyMissionService import DailyMissionService
-                DailyMissionService.start_claim_reward(socketio_instance=socketio_instance)
-                DailyMissionService.daily_mission_thread.join()
 
     @staticmethod
     def stop_one_dragon(socketio_instance=None):
         from controller.BaseController import BaseController
         BaseController.stop_listen = True
+        socketio_instance.emit(SOCKET_EVENT_ONE_DRAGON_END,'已停止一条龙')
 
 
