51e18bc5bde57614cd80f0f955726a76b6b02be7
tignioj <859897994@qq.com>
2024-10-29 04:41:30 +0800

    存入地图宽高；根据坐标初步计算出坐标所在地图

--- myexecutor/CordinateTransfer.py
+++ myexecutor/CordinateTransfer.py
@@ -19,9 +19,8 @@ def get_map_absolute_xyxy(map_name, version=0):
     from myutils.configutils import MapConfig
     allmap = MapConfig.get_all_map()
     name = allmap[map_name]['img_name']
-    img_path = os.path.join(resource_path, 'map', 'segments', f'{name}_2048_v{version}.png')
-    img = cv2.imread(img_path)
-    h, w = img.shape[:2]
+    w = allmap[map_name]['width']
+    h = allmap[map_name]['height']
     center = allmap[map_name]['center']
     x1, y1 = center_x - center[0], center_y - center[1]
     x2, y2 = x1 + w, y1 + h
@@ -51,23 +50,41 @@ def get_country_from_minimap_position(x, y):
         # 先将相对坐标转换为像素坐标
         x, y = to_abs_position(x, y)
         x1, y1, x2, y2 = get_map_absolute_xyxy(map_name)
-        print(x1, y1, x2, y2)
+        # print(x1, y1, x2, y2)
         if x1 <= x <= x2 and y1 <= y <= y2: return map_name
         return None
 
+    # TODO: 对于处于地图交叉处应当加载特征点，检测谁在该区域特征点多就返回谁
     result = within_map(x, y, '蒙德') or \
              within_map(x, y, '璃月') or \
              within_map(x, y, '须弥') or \
              within_map(x, y, '枫丹') or \
              within_map(x, y, '稻妻') or \
              within_map(x, y, '纳塔')
+    if result is None:
+        print("根据坐标获取地图名称返回值为空，可能是坐标在层岩巨渊或者渊下宫")
     return result
 
     # if x1 > x > x2 and  y1 > y > y2: print("璃月")
 
-
-def bgi2minimap(bgi_json, save_path, save=False):
+def bgi2minimap_position(bgi_position):
+    """
+    坐标转换
+    :param bgi_position:
+    :return:
+    """
     dx, dy = 790, 1241
+    x = -bgi_position[0] + dx / 2
+    y = -bgi_position[1] - dy / 2
+    return x*2,y*2
+def minimap2bgi_position(minimap_position):
+    dx, dy = 790, 1241
+    x = -minimap_position[0] / 2 + dx / 2
+    y = -minimap_position[1] / 2 - dy / 2
+    return x,y
+
+# json转换
+def bgi2minimap(bgi_json, save_path, save=False):
     with open(bgi_json, 'r', encoding='utf8') as f:
         data = json.load(f)
         new_data = dict()
@@ -85,10 +102,9 @@ def bgi2minimap(bgi_json, save_path, save=False):
                 position['move_mode'] = 'normal'
             if position.get('type') == 'teleport':
                 position['type'] = 'path'
-            x = -position.get('x') + dx / 2
-            y = -position.get('y') - dy / 2
-            position['x'] = x * 2
-            position['y'] = y * 2
+            x,y = bgi2minimap_position((position.get('x'), position.get('y')))
+            position['x'] = x
+            position['y'] = y
 
         new_data['positions'] = positions
     print(new_data)
@@ -115,8 +131,7 @@ def minimap2bgi(minimap_json, save_path, save=False):
                 position['move_mode'] = 'walk'
             if position.get('type') == 'path':
                 position['type'] = 'teleport'
-            x = -position.get('x') / 2 + dx / 2
-            y = -position.get('y') / 2 - dy / 2
+            x,y = minimap2bgi_position((position.get('x'), position.get('y')))
             position['x'] = x
             position['y'] = y
 
@@ -130,22 +145,22 @@ def minimap2bgi(minimap_json, save_path, save=False):
 
 
 def test1():
-    folder = os.path.join(resource_path, 'pathlist', '灼灼彩菊')
-    new_folder = os.path.join(resource_path, 'pathlist', 'bgi灼灼彩菊')
+    folder = os.path.join(resource_path, 'pathlist', '星螺')
+    new_folder = os.path.join(resource_path, 'pathlist', '新星螺')
     if not os.path.exists(new_folder):
         os.makedirs(new_folder)
     files = os.listdir(folder)
     for file in files:
         file_path = os.path.join(folder, file)
-        save_path = os.path.join(new_folder, f'bgi{file.split(".json")[0]}.json')
+        save_path = os.path.join(new_folder, f'新{file.split(".json")[0]}.json')
         try:
-            # bgi2minimap(bgi_json=file_path, save_path=save_path, save=True)
-            minimap2bgi(minimap_json=file_path, save_path=save_path, save=True)
+            bgi2minimap(bgi_json=file_path, save_path=save_path, save=True)
+            # minimap2bgi(minimap_json=file_path, save_path=save_path, save=True)
         except Exception as e:
             print(e)
 
-
 if __name__ == '__main__':
-    country = get_country_from_minimap_position(0, 0)
+    # country = get_country_from_minimap_position(0, 0)
     # country = get_country_from_minimap_position(1948,-4946)
-    print(country)
+    # print(country)
+    test1()
